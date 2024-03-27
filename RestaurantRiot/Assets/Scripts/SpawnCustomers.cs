d3d0c063b43f86242ebafc886384537358f74d5c
Steff1928 <prossersteve151@gmail.com>
2024-03-27 13:26:47 +1300

    Implemented customer spawning system
    Customers can now only spawn within the boundaries of the level. If not, their prefab clones get removed. Next step is to add animations and manage what happens once a customer has been fed.

--- RestaurantRiot/Assets/Scripts/SpawnCustomers.cs
+++ RestaurantRiot/Assets/Scripts/SpawnCustomers.cs
@@ -6,12 +6,22 @@ public class SpawnCustomers : MonoBehaviour
 {
     [SerializeField] GameObject[] customers;
     [SerializeField] GameObject[] enemyCustomers;
+    [SerializeField] GameObject player;
+
+    float xBoundRight = 21.5f;
+    float xBoundLeft = -25.0f;
+    float zBoundBack = 4.5f;
+    float zBoundForward = 17.0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        //InvokeRepeating(nameof(SpawnInCustomers), 5, 5);
+        InvokeRepeating(nameof(SpawnInCustomers), 3, 5);
+
+        // TODO: Spawn in customers between random intervals
+        // TODO: Ensure customers are spawned within the bounds of the level (booleans, colliders and tags)
+        // TODO: Remove a customer that is touching an object
     }
 
     // Update is called once per frame
@@ -22,8 +32,9 @@ public class SpawnCustomers : MonoBehaviour
 
     void SpawnInCustomers()
     {
-        //int customersIndex = Random.Range(0, customers.Length);
+        int customersIndex = Random.Range(0, customers.Length);
+        Vector3 spawnPos = new Vector3(Random.Range(xBoundLeft, xBoundRight), 0.1f, Random.Range(zBoundBack, zBoundForward));
 
-        //Instantiate(customers[customersIndex], new Vector3(Random.Range(-8.0f, 8.0f), 0, 8.0f), Quaternion.identity);
+        Instantiate(customers[customersIndex], spawnPos, Quaternion.identity);
     }
 }
