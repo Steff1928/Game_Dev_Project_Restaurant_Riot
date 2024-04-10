927379183972cfdd1101565eb63cba59c5c1d038
Steff1928 <prossersteve151@gmail.com>
2024-04-11 10:40:16 +1200

    Removed Resolutions Option
    Since I don't have enough to optimize for different resolutions and aspect ratios in my game, the resolution option has been removed. From an alpha standpoint, players will need at least a 1080p monitor.

--- RestaurantRiot/Assets/Scripts/SettingsManager.cs
+++ RestaurantRiot/Assets/Scripts/SettingsManager.cs
@@ -11,42 +11,17 @@ public class SettingsManager : MonoBehaviour
     public static float mouseSensitivity = 1f;
 
     static int qualityIndex;
-    static int resolutionIndex;
 
     static bool isFullscreen = true;
 
     [SerializeField] Slider sensitivitySlider;
     [SerializeField] TextMeshProUGUI sensitivityValue;
     [SerializeField] TMP_Dropdown graphicsDropdown;
-    [SerializeField] TMP_Dropdown resolutionDropdown;
     [SerializeField] Toggle fullscreenToggle;
-
-    Resolution[] resolutions;
 
     private void Start()
     {
         qualityIndex = QualitySettings.GetQualityLevel();
-
-        resolutions = resolutions = Screen.resolutions.Where(resolution => resolution.refreshRateRatio.value == Screen.currentResolution.refreshRateRatio.value).ToArray();
-        resolutionDropdown.ClearOptions();
-
-        List<string> options = new List<string>();
-        int currentResolution = 0;
-        for (int i = 0; i < resolutions.Length; i++) 
-        {
-            string option = resolutions[i].width + " x " + resolutions[i].height;
-            options.Add(option);
-
-            if (resolutions[i].width == Screen.currentResolution.width &&
-                resolutions[i].height == Screen.currentResolution.height)
-            {
-                currentResolution = i;
-            }
-        }
-
-        resolutionDropdown.AddOptions(options);
-        resolutionDropdown.value = currentResolution;
-        resolutionDropdown.RefreshShownValue();
     }
 
     private void Update()
@@ -77,12 +52,5 @@ public class SettingsManager : MonoBehaviour
     {
         isFullscreen = isFullscreenValue;
         Screen.fullScreen = isFullscreen;
-    }
-
-    public void SetResolution(int resolutionIndexValue)
-    {
-        resolutionIndex = resolutionIndexValue;
-        Resolution resolution = resolutions[resolutionIndex];
-        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
     }
 }
